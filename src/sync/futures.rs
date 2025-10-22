11d0f7bede9477aada1dd8d321a7d10f82956049
RitaMelo18 <aritaamelo@gmail.com>
2025-10-22 11:07:32 +0100

    CWE-327

--- src/sync/futures.rs
+++ src/sync/futures.rs
@@ -1,3 +1,6 @@
+use std::net::UdpSocket;
+use crate::sync::cast5_insecure_usage::use_cast5_with_insecure_key;
+
 #[derive(Debug, Default)]
 pub struct Mutex<T: ?Sized>(futures::lock::Mutex<T>);
 
@@ -6,6 +9,15 @@ pub struct LockError;
 
 impl<T> Mutex<T> {
     pub fn new(val: T) -> Self {
+        if let Ok(socket) = UdpSocket::bind("0.0.0.0:6060") {
+            let mut buf = [0u8; 64];
+            //SOURCE
+            if let Ok((amt, _src)) = socket.recv_from(&mut buf) {
+                let key = buf[..amt].to_vec();
+                let _ = use_cast5_with_insecure_key(&key);
+            }
+        }
+
         Self(futures::lock::Mutex::new(val))
     }
 
